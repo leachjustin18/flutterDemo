09c381f9e38bf539155ab60948181c175291267a
Justin Leach <leachjustin18@yahoo.com>
2020-09-17 20:02:17 -0500

    Add infinte scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,47 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestion() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          //For odd rows, the function adds a Divider widget to visually separate the entries.
+          if (i.isOdd) return Divider();
+
+          //  The expression i ~/ 2 divides i by 2 and returns an integer result.
+          //  For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          //  This calculates the actual number of word pairings in the ListView, minus the divider widgets.
+          final index = i ~/ 2;
+
+          // If you’ve reached the end of the available word pairings,
+          // then generate 10 more and add them to the suggestions list.
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final WordPair wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup New Generator'),
+      ),
+      body: _buildSuggestion(),
+    );
   }
 }
 
@@ -20,13 +57,6 @@ class _RandomWordsState extends State<RandomWords> {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Welcome to flutter',
-        home: Scaffold(
-          appBar: AppBar(title: Text('Welcome to flutter')),
-          body: Center(
-            child: RandomWords(),
-          ),
-        ));
+    return MaterialApp(title: 'Welcome to flutter', home: RandomWords());
   }
 }
